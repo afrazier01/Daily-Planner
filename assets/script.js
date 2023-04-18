41e07d2acf05a95e455680d2eced80a1c01f02ca
afrazier01 <amfrazier01@gmail.com>
2023-04-18 00:57:02 -0500

    Fixed error to properly link files

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var saveButtons = $('.saveBtn')
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,3 +23,9 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+saveButtons.on('click', function () {
+  console.log('FTRE')
+})
+
+
